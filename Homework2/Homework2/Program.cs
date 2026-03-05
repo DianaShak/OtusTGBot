43b32a89d39ad5b5e70fb0fe0d0d5932adf389aa
DianaSh <oneslimmouse@gmail.com>
2026-03-05 19:08:18 +0300

    Добавлена возможность добавлять задачи в список задач.

--- Homework2/Homework2/Program.cs
+++ Homework2/Homework2/Program.cs
@@ -1,17 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Homework2
 {
     internal class Program
     {
         const string ProgrammVersionInfo = "версия 1.0, 17.02.2026";
+        
+        public static void ShowAllTasks(List<string> tasks)
+        {
+            int taskIndex = 1;
+            if (tasks != null || tasks.Count > 0)
+            {
+                foreach (var task in tasks)
+                {
+                    Console.WriteLine($"{taskIndex++} {task}");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Список пуст.");
+            }
+        }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Привет! Выберите нужную команду!");
-            Console.WriteLine(@"/start /help /info /exit");
+            Console.WriteLine(@"/start /help /info /echo /addtask /showtasks /removetask /exit");
             var input = string.Empty;
             var name = string.Empty;
+            List<string> tasks = new List<string>();
 
             while (input != "/exit")
             {
@@ -28,11 +46,12 @@ namespace Homework2
                         //  Запрашиваем имя пользователя.
                         Console.WriteLine("Пожалуйста, введите свое имя:");
                         name = Console.ReadLine();
+                        Console.WriteLine($"Здравствуйте, {name}! Чем я могу помочь?");
                         break;
 
                     case "/help":
                         //  Отображает краткую справочную информацию о том, как пользоваться программой.
-                        Console.WriteLine($"{(string.IsNullOrWhiteSpace(name) ? "Пожалуйста, начните с команды /start." : $"{name}, команда /start запрашивает имя и открывает доступ к команде /echo, команда /help позволяет получить краткую справочную информацию о том, как пользоваться программой, команда /info позволяет получить информацию о версии программы и дате её создания, команда /echo позволяет вывести введенный Вами текст после команды, команда /exit позволяет выйти из меню.")}");
+                        Console.WriteLine($"{(string.IsNullOrWhiteSpace(name) ? "Пожалуйста, начните с команды /start." : $"{name}, команда /start запрашивает имя и открывает доступ к команде /echo, команда /help позволяет получить краткую справочную информацию о том, как пользоваться программой, команда /info позволяет получить информацию о версии программы и дате её создания, команда /echo позволяет вывести введенный Вами текст после команды, команда /addtask позволяет добавлять задачи, команда /showtasks выводит список введенных задач, команда /removetask позволяет удалить определенную задачу, команда /exit позволяет выйти из меню.")}");
                         break;   
 
                     case "/info":
@@ -44,8 +63,63 @@ namespace Homework2
                         //  После ввода имени становится доступной команда /echo.
                         //  При вводе этой команды с аргументом (например, /echo Hello), программа возвращает введенный текст (в данном примере "Hello").
                         Console.WriteLine($"{(string.IsNullOrWhiteSpace(name) ? "Пожалуйста, начните с команды /start." : $"{string.Join(' ', splitInput[1..])}")}");
-                       break;
+                        break;
 
+                    case "/addtask":
+                        Console.WriteLine("Пожалуйста, введите описание задачи:");
+                        var task = Console.ReadLine();
+                        tasks.Add(task);
+                        Console.WriteLine($"Задача {task} добавлена.");
+                        break;
+
+                    case "/showtasks":
+                        if (tasks != null && tasks.Count > 0)
+                        {
+                            Console.WriteLine("Вот Ваш список задач:");
+                            ShowAllTasks(tasks);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Список пуст.");
+                        }
+                        break;
+
+                    case "/removetask":
+                        bool valid = false;
+                        do
+                        {
+                            if (tasks != null && tasks.Count > 0)
+                            {
+                                Console.WriteLine("Введите номер задачи, которую хотите удалить:");
+                                ShowAllTasks(tasks);
+                                var taskNumberToRemove = Console.ReadLine();
+                                int taskNumber;
+
+                                if (!int.TryParse(taskNumberToRemove, out taskNumber))
+                                {
+                                    Console.WriteLine("Число не распознано.");
+                                    continue;
+                                }
+                                int indexToRemove = taskNumber - 1;
+
+                                if (indexToRemove >= 0 && indexToRemove < tasks.Count)
+                                {
+                                    valid = true;
+                                    string removedTaskName = tasks[taskNumber - 1];
+                                    tasks.RemoveAt(indexToRemove);
+                                    Console.WriteLine($"Задача {removedTaskName} удалена.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Элемент с таким номером не существует. Пожалуйста, введите корректный номер.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Список пуст.");
+                            }
+                        } while (!valid);
+                        break;
                     case "/exit":
                         break;
 
@@ -55,6 +129,7 @@ namespace Homework2
 
                 }
             }
+
         }
     }
 }
