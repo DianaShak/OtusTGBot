ab530b1f2d7739a6f3c79dac475d57e61b6b43f4
DianaSh <oneslimmouse@gmail.com>
2026-04-09 22:56:00 +0300

    Добавлены классы ToDoUser, ToDoItem. Добавлены команды /showalltasks, /completetask. Изменена логика /showtasks.

--- Homework2/Homework2/Program.cs
+++ Homework2/Homework2/Program.cs
@@ -1,23 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace Homework2
 {
     internal class Program
     {
+        static ToDoUser user;
+        
         const string ProgrammVersionInfo = "версия 1.0, 17.02.2026";
         
-        public static void ShowAllTasks(List<string> tasks)
+        public static void ShowAllTasks(List<ToDoItem> tasks, bool showAll)
         {
             int taskIndex = 1;
-            if (tasks != null || tasks.Count > 0)
+            if (tasks?.Count > 0)
             {
                 foreach (var task in tasks)
                 {
-                    Console.WriteLine($"{taskIndex++} {task}");
-                }
+                    if (showAll)
+                    {
+                        Console.WriteLine($"{taskIndex++} {task.State} {task.Name} {task.CreatedAt} {task.Id}");
+                    }
+                    else if (task.State == ToDoItemState.Active)
+                    {
+                        Console.WriteLine($"{taskIndex++} {task.Name} {task.CreatedAt} {task.Id}");
+                    }
+                } 
             }
             else 
             {
@@ -29,17 +38,22 @@ namespace Homework2
         {
             
             Console.WriteLine("Выберите нужную команду!");
-            Console.WriteLine(@"/start /help /info /echo /addtask /showtasks /removetask /exit");
+            Console.WriteLine(@"/start /help /info /echo /addtask /completetask /showtasks /showalltasks /removetask /exit");
             var input = string.Empty;
             var name = string.Empty;
-            List<string> tasks = new List<string>();
+            var tasks = new List<ToDoItem>();
             int taskCountLimit = 0;
             int taskLengthLimit = 0;
             string taskLength = string.Empty;
             int min = 1;
             int max = 100;
-            
 
+            //if (splitInput[0] != "/start" || splitInput[0] != "/help" || splitInput[0] != "/info" || splitInput[0] != "/echo" || splitInput[0] != "/addtask" || splitInput[0] != "/showtasks" || splitInput[0] != "/removetask" || splitInput[0] != "/exit")
+            //{
+            //    Console.WriteLine("Такой команды нет. Пожалуйста, введите команду из предложенных:");
+            //    Console.WriteLine(@"/start /help /info /echo /addtask /showtasks /removetask /exit");
+            //    continue;
+            //}
 
             while (input != "/exit")
             {
@@ -59,22 +73,8 @@ namespace Homework2
                     {
                         Console.WriteLine("Введите максимально допустимое количество задач (от 1 до 100):");
                         var maxNumber = Console.ReadLine();
-
-                        if (!int.TryParse(maxNumber, out taskCountLimit))
-                        {
-                            throw new ArgumentException("Введено не число.");
-                            
-                        }
-                        else if (taskCountLimit < 1 || taskCountLimit > 100)
-                        {
-                            throw new ArgumentException($"Число должно быть в диапазоне от 1 до 100. Число {taskCountLimit} не подходит.");
-
-                        }
-                        
-                        else
-                        {
-                            Console.WriteLine($"Максимальное количество задач: {taskCountLimit}");
-                        }
+                        taskCountLimit = ParseAndValidateInt(maxNumber);
+                        Console.WriteLine($"Максимальное количество задач: {taskCountLimit}");
                     }
 
                     if (taskLengthLimit == 0)
@@ -89,23 +89,25 @@ namespace Homework2
                     {
                         case "/start":
                             //  Запрашиваем имя пользователя.
-                            name = NameMethod();
+                            var userName = GetNameMethod();
+                            ToDoUser user = new ToDoUser(name);  //  1 пункт
+                            Console.WriteLine($"Здравствуйте, {userName}! Чем я могу помочь?");
                             break;
 
                         case "/help":
                             //  Отображает краткую справочную информацию о том, как пользоваться программой.
-                            HelpMethod(name);
+                            HelpMethod();
                             break;
 
                         case "/info":
                             //  Предоставляет информацию о версии программы и дате её создания.
-                            InfoMethod(name);
+                            InfoMethod();
                             break;
 
                         case "/echo":
                             //  После ввода имени становится доступной команда /echo.
                             //  При вводе этой команды с аргументом (например, /echo Hello), программа возвращает введенный текст (в данном примере "Hello").
-                            EchoMethod(name, splitInput);
+                            EchoMethod(splitInput);
                             break;
 
                         case "/addtask":
@@ -118,6 +120,26 @@ namespace Homework2
 
                         case "/removetask":
                             RemoveTaskMethod(tasks);
+                            break;
+                        case "/completetask":
+                            //Найти задачу по Id
+                            //Обновить State на ToDoItemState.Completed
+                            //Обновить StateChangedAt
+                            //Пример: / completetask 73c7940a - ca8c - 4327 - 8a15 - 9119bffd1d5e
+                            var idToFind = string.Join(' ', splitInput[1..]);
+                            ValidateString(idToFind);
+                            if (Guid.TryParse(idToFind, out Guid idSearch));
+                            {
+                                ToDoItem foundTask = tasks.Find(t => t.Id == idSearch);
+                                foundTask.State = ToDoItemState.Completed;
+                                foundTask.StateChangedAt = DateTime.UtcNow;
+                                Console.WriteLine("Задача завершена.");
+                            }
+                            break;
+                        case "/showalltasks":
+                            //Добавить обработку новой команды / showalltasks.По ней выводить команды с любым State и добавить State в вывод
+                            //Пример: (Active)Имя задачи - 01.01.2025 00:00:00 - ffbfe448 - 4b39 - 4778 - 98aa - 1aed98f7eed8
+                            ShowAllTasks(tasks, true);
                             break;
 
                         case "/exit":
@@ -155,22 +177,22 @@ namespace Homework2
             }
         }
 
-        private static void EchoMethod(string? name, string[] splitInput)
+        private static void EchoMethod(string[] splitInput)
         {
-            Console.WriteLine($"{(string.IsNullOrWhiteSpace(name) ? "Пожалуйста, начните с команды /start." : $"{string.Join(' ', splitInput[1..])}")}");
+            Console.WriteLine($"{((user == null) ? "Пожалуйста, начните с команды /start." : $"{string.Join(' ', splitInput[1..])}")}");
         }
 
-        private static void InfoMethod(string? name)
+        private static void InfoMethod()
         {
-            Console.WriteLine($"{(string.IsNullOrWhiteSpace(name) ? "Пожалуйста, начните с команды /start." : $"{name}, версия и дата создания: {ProgrammVersionInfo}")}");
+            Console.WriteLine($"{((user == null) ? "Пожалуйста, начните с команды /start." : $"{user?.TelegramUserName}, версия и дата создания: {ProgrammVersionInfo}")}");
         }
 
-        private static void HelpMethod(string? name)
+        private static void HelpMethod()
         {
-            Console.WriteLine($"{(string.IsNullOrWhiteSpace(name) ? "Пожалуйста, начните с команды /start." : $"{name},\nкоманда /start запрашивает имя и открывает доступ к команде /echo,\nкоманда /help позволяет получить краткую справочную информацию о том, как пользоваться программой,\nкоманда /info позволяет получить информацию о версии программы и дате её создания,\nкоманда /echo позволяет вывести введенный Вами текст после команды,\nкоманда /addtask позволяет добавлять задачи,\nкоманда /showtasks выводит список введенных задач,\nкоманда /removetask позволяет удалить определенную задачу,\nкоманда /exit позволяет выйти из меню.")}");
+            Console.WriteLine($"{((user == null) ? "Пожалуйста, начните с команды /start." : $"{user?.TelegramUserName},\nкоманда /start запрашивает имя и открывает доступ к команде /echo,\nкоманда /help позволяет получить краткую справочную информацию о том, как пользоваться программой,\nкоманда /info позволяет получить информацию о версии программы и дате её создания,\nкоманда /echo позволяет вывести введенный Вами текст после команды,\nкоманда /addtask позволяет добавлять задачи, \nкоманда /completetask меняет статус задачи на completed, \nкоманда /showtasks выводит список введенных задач со статусом Active, \nкоманда /showalltasks выводит список всех введенных задач, \nкоманда /removetask позволяет удалить определенную задачу,\nкоманда /exit позволяет выйти из меню.")}");
         }
 
-        private static void RemoveTaskMethod(List<string> tasks)
+        private static void RemoveTaskMethod(List<ToDoItem> tasks)
         {
             bool valid = false;
             do
@@ -178,7 +200,7 @@ namespace Homework2
                 if (tasks != null && tasks.Count > 0)
                 {
                     Console.WriteLine("Введите номер задачи, которую хотите удалить:");
-                    ShowAllTasks(tasks);
+                    ShowAllTasks(tasks, true);
                     var taskNumberToRemove = Console.ReadLine();
                     int taskNumber;
 
@@ -192,7 +214,7 @@ namespace Homework2
                     if (indexToRemove >= 0 && indexToRemove < tasks.Count)
                     {
                         valid = true;
-                        string removedTaskName = tasks[taskNumber - 1];
+                        var removedTaskName = tasks[taskNumber - 1];
                         tasks.RemoveAt(indexToRemove);
                         Console.WriteLine($"Задача '{removedTaskName}' удалена.");
                     }
@@ -208,12 +230,12 @@ namespace Homework2
             } while (!valid);
         }
 
-        private static void ShowTasksMethod(List<string> tasks)
+        private static void ShowTasksMethod(List<ToDoItem> tasks)
         {
             if (tasks != null && tasks.Count > 0)
             {
                 Console.WriteLine("Вот Ваш список задач:");
-                ShowAllTasks(tasks);
+                ShowAllTasks(tasks, false);
             }
             else
             {
@@ -221,7 +243,7 @@ namespace Homework2
             }
         }
 
-        private static void AddTaskMethod(List<string> tasks, int taskCountLimit, int taskLengthLimit)
+        private static void AddTaskMethod(List<ToDoItem> tasks, int taskCountLimit, int taskLengthLimit)
         {
             bool isValid = false;
             if (tasks.Count >= taskCountLimit)
@@ -234,33 +256,32 @@ namespace Homework2
                 Console.WriteLine("Пожалуйста, введите описание задачи:");
                 var task = Console.ReadLine();
                 int length = task.Length;
-                string containsTask;
                 ValidateString(task);
+                bool alreadyExist = tasks.Any(t => t.Name == task);
 
-                if (task.Length >= taskLengthLimit)
+                if (task.Length > taskLengthLimit)
                 {
                     throw new TaskLengthLimitException(task.Length, taskLengthLimit);
                 }
-                else if (tasks.Contains(task))
+                else if (alreadyExist)
                 {
                     throw new DuplicateTaskException(task);
                 }
                 else
                 {
                     isValid = true;
-                    tasks.Add(task);
-
-                    Console.WriteLine($"Задача '{task}' добавлена.");
+                    tasks.Add(new ToDoItem(user, task));  //  2 пункт
+                    Console.WriteLine($"Задача '{task}' добавлена.");  //  2 пункт
                 }
             } while (!isValid);
         }
 
-        private static string? NameMethod()
+        public static string? GetNameMethod()
         {
-            string? name;
             Console.WriteLine("Пожалуйста, введите свое имя:");
-            name = Console.ReadLine();
-            Console.WriteLine($"Здравствуйте, {name}! Чем я могу помочь?");
+            var name = Console.ReadLine();
+            ValidateString(name);
+            
             return name;
         }
 
@@ -278,11 +299,15 @@ namespace Homework2
             return strInt;
         }
 
-        private static void ValidateString(string? str)
+        private static string ValidateString(string? str)
         {
             if (string.IsNullOrWhiteSpace(str))
             {
                 throw new ArgumentException("Вы ничего не ввели.");
+            }
+            else
+            {
+                return str;
             }
         }
     }
---
+++ Homework2/Homework2/ToDoUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Homework2
+{
+    public class ToDoUser
+    {
+        
+        public Guid UserId { get; set; }
+        public string TelegramUserName { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        public ToDoUser(string telegramUserName)
+        {
+            telegramUserName = TelegramUserName;
+            RegisteredAt = DateTime.UtcNow;
+            UserId = Guid.NewGuid();
+        }
+            
+        
+    }
+
+}
